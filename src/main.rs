fadc319739666fec4100204fabdb0bc4f2e9c755
MoeShin <hi@moeshin.com>
2023-03-15 23:32:02 +0800

    refactor: HTTP method and protocol

--- src/main.rs
+++ src/main.rs
@@ -106,85 +106,93 @@ fn he_write(r: io::Result<usize>) {
 
 fn handle_tcp_stream(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    let mut headers_end_flag: u8 = 0;
-    let mut is_headers_end = false;
+    let mut headers_flag: u8 = 0;
+    let mut has_headers = false;
     let mut start_line = Vec::new();
-    let mut is_start_line_end = false;
-    let mut has_method = false;
+    let mut has_request_line = false;
+    'read:
     loop {
         match stream.read(&mut buffer) {
             Ok(0) => break,
             Ok(mut n) => {
+                let mut offset = 0;
                 for i in 0..n {
                     let byte = buffer[i];
-                    if !is_start_line_end {
+                    if !has_request_line {
                         start_line.push(byte);
-                        if !has_method && byte == b' ' {
-                            has_method = true;
-
-                            let method = String::from_utf8_lossy(
-                                &start_line[..start_line.len()])
-                                .trim_start().to_ascii_uppercase();
-                            if unsafe {
-                                (match A_DISALLOWED_METHODS.as_ref() {
-                                    None => {false}
-                                    Some(set) => {set.contains(&method)}
-                                }) || (match A_ALLOWED_METHODS.as_ref() {
-                                    None => {false}
-                                    Some(set) => {!set.contains(&method)}
-                                })
-                            } {
-                                println!("disallowed");
-                                he_write(stream.write("\
-                                HTTP/1.1 405 Method not allowed\
-                                \r\n\r\n".as_bytes()));
-                                return
-                            }
-                            he_write(stream.write("\
-                            HTTP/1.1 200 OK\r\n\
-                            Content-Type: text/plain; charset=utf-8\r\n\
-                            \r\n".as_bytes()));
-                            if method == "HEAD" {
-                                continue;
-                            }
-                            he_write(stream.write("Hello: ".as_bytes()));
-                        }
                     }
-                    if headers_end_flag == 0 && byte == b'\r' {
-                        headers_end_flag = 1;
+                    if headers_flag == 0 && byte == b'\r' {
+                        headers_flag = 1;
                         continue;
                     }
-                    let crlf_mod = headers_end_flag % 2;
+                    let crlf_mod = headers_flag % 2;
                     if !(crlf_mod == 0 && byte == b'\r') && !(crlf_mod == 1 && byte == b'\n') {
-                        headers_end_flag = 0;
+                        headers_flag = 0;
                         if byte == b'\r' {
-                            headers_end_flag = 1;
+                            headers_flag = 1;
                         }
                         continue;
                     }
-                    if !is_start_line_end && crlf_mod == 1 {
-                        is_start_line_end = true;
+                    if !has_request_line && crlf_mod == 1 {
+                        has_request_line = true;
                         unsafe {
                             start_line.set_len(start_line.len() - 2);
                         }
                         let start_line = String::from_utf8_lossy(&start_line);
                         println!("{}", start_line);
+                        let arr: Vec<&str> = start_line.split(' ')
+                            .filter(|&s| !s.is_empty()).collect();
+                        if arr.len() != 3 {
+                            eprintln!("Error HTTP request line: {}", start_line);
+                            he_write(stream.write(
+                                b"HTTP/1.1 500 Internal Server Error"));
+                            break 'read;
+                        }
+                        let method = arr[0].to_ascii_uppercase();
+                        let protocol = arr[2];
+                        if unsafe {
+                            (match A_DISALLOWED_METHODS.as_ref() {
+                                None => {false}
+                                Some(set) => {set.contains(&method)}
+                            }) || (match A_ALLOWED_METHODS.as_ref() {
+                                None => {false}
+                                Some(set) => {!set.contains(&method)}
+                            })
+                        } {
+                            println!("disallowed");
+                            he_write(stream.write(format!("\
+                            {} 405 Method not allowed\
+                            \r\n\r\n", protocol).as_bytes()));
+                            break 'read;
+                        }
+                        he_write(stream.write(format!("\
+                        {} 200 OK\r\n\
+                        Content-Type: text/plain; charset=utf-8\r\n\
+                        \r\n", protocol).as_bytes()));
+                        if method == "HEAD" {
+                            break 'read;
+                        }
+                        he_write(stream.write(b"Hello HTTP\n\n"));
+                        he_write(stream.write(start_line.as_bytes()));
+                        offset = i;
                     }
-                    if headers_end_flag > 2 {
-                        n = i - 3;
-                        is_headers_end = true;
+                    if headers_flag > 2 {
+                        n = i;
+                        has_headers = true;
                         break;
                     }
-                    headers_end_flag += 1;
+                    headers_flag += 1;
                 }
-                he_write(stream.write(&buffer[0..n]));
-                if is_headers_end {
-                    break;
+                if has_request_line {
+                    he_write(stream.write(&buffer[offset..n]));
+                }
+                if has_headers {
+                    break 'read;
                 }
             },
             Err(e) => {
                 eprintln!("Error reading from TcpStream: {}", e);
-                break;
+                break 'read;
             }
         }
     }
